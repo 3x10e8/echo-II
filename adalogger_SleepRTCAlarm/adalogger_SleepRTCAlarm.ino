593d0c8109db4f08325f0d46bacb55c693d4d23a
Abhinav Uppal <uppal@gatech.edu>
2016-05-26 02:06:59 -0400

    Mutiple alarms working w/o standbyMode()
    Added sync characters for more robust packet transmission. Resolved TRx
    buffer size issue. Added code to test alarm times with host. Made
    cleaner.

--- adalogger_SleepRTCAlarm/adalogger_SleepRTCAlarm.ino
+++ adalogger_SleepRTCAlarm/adalogger_SleepRTCAlarm.ino
@@ -1,5 +1,6 @@
 /*
   Echo II: Configuring RTC Alarm for Adafruit Adalogger over Serial
+    Used with simpleAlarmConfig running on the host PC (Processing 3)
 
     Receives current time and possible alarm times over Serial.
     Transmits back the received data.
@@ -7,7 +8,7 @@
     Issues:
       Time appears to be sync'd within seconds (can be improved)
       Error checking can be introduced to make receiving information reliable
-      Only the first configured alarm appears to work, not sure why the rest fail
+      Problems with putting the processor to sleep - only one alarm works when Serial used!
 
   Adapted from:
   http://arduino.cc/en/Tutorial/SleepRTCAlarm by Arturo Guadalupi
@@ -20,113 +21,131 @@
 #define grnLED 8
 boolean ledStatus = false;
 
-#define alSize 6 //+hhmmssDDMMYY
-#define numAlarmsMax 30
+#define alSize 6 // hhmmssDDMMYY data stored per each alarm (6 bytes)
+#define numAlarmsMax 30 // can be increased to fill unused memory
 #define rxBufferSize 180 // alSize * numAlarmsMax
-byte rxBuffer[rxBufferSize];
-int bytesRead = 0;
+
+byte rxBuffer[rxBufferSize]; // use flash memory: https://github.com/cmaglie/FlashStorage
+int bytesStored = 0;
 
 int alarmNdx = 1; // ndx 0 is used to set the time
 
-RTCZero rtc; // create an rtc object 
+RTCZero rtc; // create an rtc object
 
 void setup() {
-  Serial.begin(9600); // might help with debugging SD writes
-  //while (!Serial);
+  
+  Serial.begin(9600);
+    //while (!Serial);
   pinMode(redLED, OUTPUT); // configure as PWM?
   pinMode(grnLED, OUTPUT);
-  digitalWrite(redLED, 0);
-  digitalWrite(grnLED, 1);
-  serialEventOnSetup();
+
+  serialEventOnSetup(); // couldn't get serialEvent to work...
+  
 }
 
-void loop(){
-  //  rtc.standbyMode();    // Sleep until next alarm match
-}
+void loop() {}
 
 void alarmMatch() { // delay() doesn't work in ISRs...?
+  
+  Serial.print('@');
   ledStatus = !ledStatus;
+  //Serial.begin(9600); while(!Serial);
+  //Serial.println();
   digitalWrite(grnLED, ledStatus);
   digitalWrite(redLED, !ledStatus);
-  
-  // enable recording
 
-  // setting the next alarm fails?
-    //if (alarmNdx <= (int)bytesRead/(int)alSize) 
-    //RTC->MODE2.INTFLAG.bit.ALARM0 = 1; // clear flag
-    //RTC->MODE2.INTENSET.reg |= RTC_MODE2_INTENSET_ALARM0;
-    //RTC->MODE2.INTFLAG.reg = RTC_MODE2_INTFLAG_ALARM0; 
-    //rtc.begin();
-    //rtc.enableAlarm(rtc.MATCH_YYMMDDHHMMSS); // not needed every time?
-    //rtc.attachInterrupt(alarmMatch);
+  // enable recording
   
-  if ((alarmNdx +1)*alSize < bytesRead) alarmConfig(); // configure next alarm
+  // configure next alarm
+  if ((alarmNdx +1)*alSize < bytesStored) alarmConfig(); 
+  
 }
 
-void timeConfig() {
-  int i = 0;
+void timeConfig() { // set RTC time based on the received data
   
   rtc.begin();
+
+  int i = 0;
+  rtc.setTime(rxBuffer[i + 0], rxBuffer[i + 1], rxBuffer[i + 2]); // hh, mm, ss
+  rtc.setDate(rxBuffer[i + 3], rxBuffer[i + 4], rxBuffer[i + 5]); // DD, MM, YY
+
   rtc.enableAlarm(rtc.MATCH_YYMMDDHHMMSS); // not needed every time?
   rtc.attachInterrupt(alarmMatch);
   
-  rtc.setTime(rxBuffer[i+0], rxBuffer[i+1], rxBuffer[i+2]); // hh, mm, ss
-  rtc.setDate(rxBuffer[i+3], rxBuffer[i+4], rxBuffer[i+5]); // DD, MM, YY
 }
 
-void alarmConfig() {
-  int i = alarmNdx*alSize;
+void alarmConfig() { // set alarms on startup or with each alarm trigger
   
-  rtc.setAlarmTime(rxBuffer[i+0], rxBuffer[i+1], rxBuffer[i+2]); // hh, mm, ss
-  rtc.setAlarmDate(rxBuffer[i+3], rxBuffer[i+4], rxBuffer[i+5]); // DD, MM, YY
- 
+  int i = alarmNdx * alSize; //Serial.println(i);
+  rtc.setAlarmTime(rxBuffer[i + 0], rxBuffer[i + 1], rxBuffer[i + 2]); // hh, mm, ss
+  rtc.setAlarmDate(rxBuffer[i + 3], rxBuffer[i + 4], rxBuffer[i + 5]); // DD, MM, YY
+  
   alarmNdx++;
+
+  /*
+    Serial.write(rtc.getAlarmHours()); //Serial.write('_');
+    Serial.write(rtc.getAlarmMinutes()); //Serial.write('_');
+    Serial.write(rtc.getAlarmSeconds()); //Serial.write('`');
+    Serial.write(rtc.getAlarmMonth()); //Serial.write('|');
+    Serial.write(rtc.getAlarmDay()); //Serial.write('|');
+    Serial.write(rtc.getAlarmYear());
+  */
+
+  //rtc.standbyMode(); <--- wooo badddd. Need to test this w/o USB.
   
-  // "Set alarm for ";
-  Serial.write(rtc.getAlarmHours()); Serial.write(':');
-  Serial.write(rtc.getAlarmMinutes()); Serial.write(':');
-  Serial.write(rtc.getAlarmSeconds()); Serial.write(' ');
-  Serial.write(rtc.getAlarmMonth()); Serial.write('/');
-  Serial.write(rtc.getAlarmDay()); Serial.write('/'); 
-  Serial.write(rtc.getAlarmYear());
-  
-  rtc.standbyMode();
 }
 
-void serialEventOnSetup() { // couldn't get serialEvent to work...
-  digitalWrite(grnLED, 0); //Serial.println("Waiting to be configured...");
+void serialEventOnSetup() {
   
-  int waitInterval = millis();
+  //___,_|_,-.____,_|_,-.____,_|_,-.____,_|_,-.__//
+  // This is redundant: just to have a nice wait //
+  // heartBeat() // somehow messes up call to Serial.write()
+  
+  int waitCounter = millis(), waitFor = 450, toFlip = 200;
   while (Serial.available() == 0 && Serial.read() != '$') { // waiting to receive data
-    digitalWrite(redLED, 0); delayMicroseconds(millis() -waitInterval); digitalWrite(redLED, 1);
-    if ((millis() -waitInterval) > 500) { //Serial.println("waiting...");
-      waitInterval = millis(); 
+    digitalWrite(redLED, 0); delayMicroseconds(millis() - waitCounter); digitalWrite(redLED, 1);
+    if ((millis() - waitCounter) > waitFor) { //Serial.println("waiting...");
+      waitCounter = millis();
+      toFlip *= -1;
+      waitFor += toFlip;
     }
   }
-  digitalWrite(redLED, ledStatus);
+  digitalWrite(redLED, ledStatus); // off
 
-  bytesRead = Serial.available(); // includes '$' and '\n', hence -2 later
-  // Serial.println(bytesRead); // convert ASCII to int, read until '\n' (which is 1310)
-  Serial.read(); // '$'
-  Serial.readBytes(rxBuffer, bytesRead -2);
-  Serial.write(rxBuffer, bytesRead -2);
-  
-  timeConfig(); // apparently need rtc.begin() first else alarm write syncs won't work...
-  alarmConfig(); // each alarm setting is 6 bytes
-  
-  /*
-  bytesRead = 0;
-  while (numPackets>0) { 
-    bytesRead += Serial.readBytesUntil('>', rxBuffer+bytesRead, sizeof(byte)*alSize);
-    numPackets--;
-    //delay(1);
+  while (Serial.read() == '$'); // clear the header
+
+  int bytesRxd = 0; // store received data to memory
+  while (Serial.available()) { // to deal with the 64 byte s/w rx buffer
+    bytesRxd = Serial.available();
+      //Serial.println(bytesRxd);
+      //Serial.println(bytesStored); // convert ASCII to int, read until '\n' (which is 1310)
+    Serial.readBytes(rxBuffer +bytesStored, bytesRxd); // get the date/time data
+    bytesStored += bytesRxd;
   }
-  Serial.write(rxBuffer, bytesRead);
-  */
+
+  int bytesTxd = 0;//, i;
+  while (bytesTxd < bytesStored) { // just to check the data
+      // i = Serial.write(rxBuffer[bytesTxd]); // i returned 48 (decimal 0)...what...
+    Serial.write(rxBuffer[bytesTxd]);
+    bytesTxd++;
+  }
+
+  timeConfig(); // apparently need rtc.begin() first else alarm write syncs won't work...
+  alarmConfig();
   
-  //byte numPackets[1];
-          //int numPackets_int = (int)Serial.read();
-  //Serial.readBytesUntil('\n', numPackets, 1);
-  //int numPackets_int = (int) numPackets;
-          //Serial.println(numPackets_int);
 }
+
+void heartBeat(){
+  
+  int waitCounter = millis(), waitFor = 450, toFlip = 200;
+  while (Serial.available() == 0 && Serial.read() != '$') { // waiting to receive data
+    digitalWrite(redLED, 0); delayMicroseconds(millis() - waitCounter); digitalWrite(redLED, 1);
+    if ((millis() - waitCounter) > waitFor) { //Serial.println("waiting...");
+      waitCounter = millis();
+      toFlip *= -1;
+      waitFor += toFlip;
+    }
+  }
+  
+}
+
